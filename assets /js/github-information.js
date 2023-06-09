357e294de36ad7a8b0a729f52ee023d0aa5eb5eb
Hayespace <ben.hayes@hotmail.com>
2023-06-09 15:51:16 +0000

    add error function

--- assets /js/github-information.js
+++ assets /js/github-information.js
@@ -17,6 +17,14 @@ function fetchGitHubInformation(event) {
     function(response) {
         var userData = response;
         $("#gh-user-data").html(userInformationHTML(userData));
+    }, function(errorResponse) {
+        if (errorResponse.status === 404) {
+            $("#gh-user-data").html(`<h2>No info found for user ${username}</h2>`);
+        } else {
+            console.log(errorResponse);
+            $("#gh-user-data").html(
+                `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
+        }
     })
 }
 
